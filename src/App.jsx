c04b418acef8de4ced8d696396523bdb99d28d95
Niloy11111 <Ektiaruddinniloy859@gmail.com>
2023-09-14 23:10:05 +0600

    added the heading of navbar and setup google font

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='px-16 py-14 bg-[#F3F3F3]'>
+      <h1 className='text-3xl text-center text-[#1C1B1B] font-bold'>Course Registration</h1>
+    </div>
+  )
+}
+
+export default App
